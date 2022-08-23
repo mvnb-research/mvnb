1441178cf683c47b6db30e96bbfda59c52ffa52d
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2022-08-23 23:26:51 +0900

    Fix config tests

--- test/test_config.py
+++ test/test_config.py
@@ -14,6 +14,9 @@ def test_default_values():
     assert config.repl == [executable, "-i", _bootstrap.__file__]
     assert config.fork == "__fork('__address__')"
     assert config.callback == "__callback('__url__', '__message__')"
+    assert config.fork_addr == "__address__"
+    assert config.callback_url == "__url__"
+    assert config.callback_message == "__message__"
 
 
 def test_help(capsys_test):
@@ -23,15 +26,19 @@ def test_help(capsys_test):
     capsys_test(
         """
 usage: mvnb [--help] [--version] [--addr <addr>] [--port <port>] [--repl <repl>]
-            [--fork <code>] [--callback <code>]
+            [--fork <code>] [--callback <code>] [--fork-addr <text>]
+            [--callback-url <text>] [--callback-message <text>]
 
 options:
-  --help             show help
-  --version          show version
-  --addr <addr>      server address
-  --port <port>      server port
-  --repl <repl>      repl command
-  --fork <code>      fork code
-  --callback <code>  callback code
+  --help                     show help
+  --version                  show version
+  --addr <addr>              server address
+  --port <port>              server port
+  --repl <repl>              repl command
+  --fork <code>              fork code
+  --callback <code>          callback code
+  --fork-addr <text>         fork address placeholder
+  --callback-url <text>      callback url placeholder
+  --callback-message <text>  callback message placeholder
 """
     )
