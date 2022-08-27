c5b083e853f959e87d423a50725d52f0f3d3c477
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2022-08-27 21:11:41 +0900

    Add react-flow

--- mvnb-gui/src/index.tsx
+++ mvnb-gui/src/index.tsx
@@ -1,18 +1,7 @@
+import { App } from "./app";
+import "normalize.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { python } from "@codemirror/lang-python";
-import CodeMirror from "@uiw/react-codemirror";
-import "normalize.css";
-
-const App = () => {
-  return (
-    <CodeMirror
-      value="print('Hello, world!')"
-      height="100vh"
-      extensions={[python()]}
-    />
-  );
-};
 
 const app = <App />;
 
