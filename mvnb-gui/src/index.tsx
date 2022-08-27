0b5ccb2ee0af8fdcf62b0ab58108a33dc37b271b
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2022-08-27 18:51:08 +0900

    Add React and CodeMirror

--- mvnb-gui/src/index.tsx
+++ mvnb-gui/src/index.tsx
@@ -1,1 +1,23 @@
-console.log("mvnb gui!");
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { python } from "@codemirror/lang-python";
+import CodeMirror from "@uiw/react-codemirror";
+import "normalize.css";
+
+const App = () => {
+  return (
+    <CodeMirror
+      value="print('Hello, world!')"
+      height="100vh"
+      extensions={[python()]}
+    />
+  );
+};
+
+const app = <App />;
+
+const div = document.getElementById("app");
+
+const root = createRoot(div!);
+
+root.render(<StrictMode>{app}</StrictMode>);
