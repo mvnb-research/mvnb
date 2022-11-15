b2ed3f9359a000b57d50a2d749ba3fd7656580c2
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2022-11-15 13:38:59 +0900

    Add panel

--- mvnb-gui/src/board.tsx
+++ mvnb-gui/src/board.tsx
@@ -1,3 +1,8 @@
+import { Panel } from "./panel";
 import ReactFlow from "react-flow-renderer";
 
-export const Board = () => <ReactFlow></ReactFlow>;
+export const Board = () => (
+  <ReactFlow>
+    <Panel />
+  </ReactFlow>
+);
---
+++ mvnb-gui/src/panel.tsx
@@ -0,0 +1,19 @@
+import AddIcon from "@mui/icons-material/Add";
+import SaveIcon from "@mui/icons-material/Save";
+import { ButtonGroup, Card, IconButton } from "@mui/material";
+
+export const Panel = () => (
+  <Card
+    variant="outlined"
+    style={{ position: "absolute", top: 10, right: 10, zIndex: 99 }}
+  >
+    <ButtonGroup orientation="vertical">
+      <IconButton color="primary">
+        <SaveIcon />
+      </IconButton>
+      <IconButton color="primary">
+        <AddIcon />
+      </IconButton>
+    </ButtonGroup>
+  </Card>
+);
