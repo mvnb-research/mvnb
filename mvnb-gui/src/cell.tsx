9ffe5e13b4a2c73306105df6e5348059c76e6575
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2022-11-16 21:50:32 +0900

    Fix cell view

--- mvnb-gui/src/cell.tsx
+++ mvnb-gui/src/cell.tsx
@@ -1,25 +1,13 @@
 import * as client from "./client";
-import * as state from "./state";
 import { Cell } from "./types";
 import { python } from "@codemirror/lang-python";
 import { Delete, PlayCircle } from "@mui/icons-material";
 import { Card, CardActions, CardContent, IconButton } from "@mui/material";
 import { Box } from "@mui/system";
 import ReactCodeMirror from "@uiw/react-codemirror";
-import { useState } from "react";
 import { Handle, NodeProps, Position } from "react-flow-renderer";
 
 export const CellView = (props: NodeProps<Cell>) => {
-  const [source, setSource] = useState(props.data.source ?? "");
-  const [outputs, setOutputs] = useState(props.data.outputs);
-  const [editable, setEditable] = useState(state.isEditable(props.id));
-  const [runnable, setRunnable] = useState(state.isRunnable(props.id));
-  const [deletable, setDeletable] = useState(state.isDeletable(props.id));
-  state.addSetSource(props.id, setSource);
-  state.addSetOutputs(props.id, setOutputs);
-  state.addSetEditable(props.id, setEditable);
-  state.addSetRunnable(props.id, setRunnable);
-  state.addSetDeletable(props.id, setDeletable);
   return (
     <Box width={`${cellWidth}vw`}>
       {props.data.parent != null && (
@@ -30,29 +18,29 @@ export const CellView = (props: NodeProps<Cell>) => {
           <IconButton
             color="primary"
             onClick={() => client.runCell(props.id)}
-            disabled={!runnable}
+            disabled={!props.data.runnable}
           >
             <PlayCircle />
           </IconButton>
           <IconButton
             color="primary"
             onClick={() => client.deleteCell(props.id)}
-            disabled={!deletable}
+            disabled={!props.data.deletable}
           >
             <Delete />
           </IconButton>
         </CardActions>
         <CardContent sx={{ pt: 0 }}>
           <ReactCodeMirror
-            value={source}
+            value={props.data.source ?? ""}
             extensions={[python()]}
-            editable={editable}
+            editable={props.data.editable}
             onChange={(value, viewUpdate) => {
               client.updateCell(props.id, value);
             }}
           />
         </CardContent>
-        {outputs.length > 0 && (
+        {props.data.outputs.length > 0 && (
           <CardContent
             sx={{
               fontFamily: "Monospace",
@@ -64,7 +52,7 @@ export const CellView = (props: NodeProps<Cell>) => {
               "&:last-child": { pb: "16px" },
             }}
           >
-            {outputs.map((o) => {
+            {props.data.outputs.map((o) => {
               return <Box key={o.id}>{o.data}</Box>;
             })}
           </CardContent>
